de1d3f2c81a9191bc7404cc793c253c35691842e
Azamat Karimov <azamat-karimov-05@mail.ru>
2026-09-21 23:58:24 +0300

    Add hello action and root route

---
+++ lab1/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  def hello
+    render plain: "hello, world!"
+  end
+end
