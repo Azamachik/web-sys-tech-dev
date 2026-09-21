74c8b957ce378537b87e3fffcbb36e0a8575be2d
Azamat Karimov <azamat-karimov-05@mail.ru>
2026-09-22 00:04:39 +0300

    Add non-ASCII support to hello

--- lab1/app/controllers/application_controller.rb
+++ lab1/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   def hello
-    render plain: "hola, mundo!"
+    render plain: "¡Hola, mundo!"
   end
 end
